4901eaea9bcb6fb23bf3f93fecf67f4cf28d9503
oceenachi <phillipfrancisca.com>
2024-01-09 17:53:48 +0100

    Implementation of a service that computes daily stocks transaction volume

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.postgresql:postgresql")
     implementation ("io.github.microutils:kotlin-logging-jvm:2.0.11")
-    implementation ("ch.qos.logback:logback-classic:1.2.6")
+    implementation ("ch.qos.logback:logback-classic:1.4.12")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
